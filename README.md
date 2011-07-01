57ade2c0bb455cc76423d0bda7859aa3355afa4d
Thomas Dudziak <thomas@ning.com>
2011-07-01 14:45:33 -0700

    Initial checkin

---
+++ README.md
@@ -0,0 +1,37 @@
+## Introduction <a name="introduction"></a>
+
+The `OracleSchemaDumper` is a simple Java tool that dump the DDL and partial DML for specified schemas (users) from an Oracle database.
+More specifically it currently dumps this info (in this order):
+
+* user creation SQL statements including permissions
+* table creation SQL statements in the correct foreign key order
+* SQL statements to create views, indexes, sequences, procedures, functions
+* INSERT statements for all rows of specified tables
+
+All these statements use the `/` character as the delimiter in order to make the SQL file usable via Sql*Plus.
+
+The database user that is used to run the tool, has to have SELECT permissions on these system tables:
+
+* `all_users`
+* `all_constraints`
+* `all_objects`
+* all tables for which INSERT statements should be generated
+
+## Usage <a name="usage"></a>
+
+  Usage: OracleSchemaDumper [options]
+    Options:
+      -d, --data-tables   Names of the tables for which data shall be exported, too
+      -f, --file          The file to write the DDL SQL to; stdout if not specified
+    * -j, --jdbc-url      Database jdbc url
+    * -p, --password      Database password
+      -s, --schema        Database schemas to export; if not specified uses the schema for the database user
+    * -u, --username      Database username
+
+## Known limitations <a name="limitations"></a>
+
+The tool currently cannot handle circular foreign key dependencies between separate tables (self references are fine).
+
+The tool currently will export binary and BLOB/CLOB columns as if they are strings, which can cause illegal characters or too-long strings
+to be present in the generated INSERT statements.
+
---
+++ src/main/java/com/ning/oracleschemadumper/OracleSchemaDumper.java
@@ -0,0 +1,364 @@
+package com.ning.oracleschemadumper;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Types;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.jgrapht.alg.CycleDetector;
+import org.jgrapht.graph.DefaultDirectedGraph;
+import org.jgrapht.graph.DefaultEdge;
+import org.jgrapht.traverse.TopologicalOrderIterator;
+import org.skife.jdbi.v2.DBI;
+import org.skife.jdbi.v2.Handle;
+import org.skife.jdbi.v2.IDBI;
+import org.skife.jdbi.v2.ResultIterator;
+import org.skife.jdbi.v2.tweak.HandleCallback;
+import org.skife.jdbi.v2.util.StringMapper;
+
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
+
+public class OracleSchemaDumper
+{
+    private static final List<String> NON_TABLE_TYPES = Arrays.asList("VIEW", "INDEX", "SEQUENCE", "PROCEDURE", "FUNCTION");
+    private static final SimpleDateFormat ORACLE_DATE_FORMAT = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+
+    @Parameter(names = { "-j", "--jdbc-url" }, description = "Database jdbc url", required = true)
+    private String jdbcUrl;
+    @Parameter(names = { "-u", "--username" }, description = "Database username", required = true)
+    private String userName;
+    @Parameter(names = { "-p", "--password" }, description = "Database password", required = true)
+    private String password;
+    @Parameter(names = { "-s", "--schema" }, description = "Database schemas to export; if not specified uses the schema for the database user")
+    private List<String> schemas;
+    @Parameter(names = { "-f", "--file" }, description = "The file to write the DDL SQL to; stdout if not specified")
+    private String outputFile;
+    @Parameter(names = { "-d", "--data-tables" }, description = "Names of the tables for which data shall be exported, too")
+    private List<String> dataTables;
+
+    private String schemaSelector;
+
+    public static void main(String[] args) throws IOException
+    {
+        final OracleSchemaDumper dumper = new OracleSchemaDumper();
+        final JCommander commander = new JCommander(dumper);
+
+        commander.setProgramName(OracleSchemaDumper.class.getSimpleName());
+        try {
+            commander.parse(args);
+            dumper.init();
+            dumper.run();
+        }
+        catch (ParameterException ex) {
+            System.out.println(ex.getMessage());
+            commander.usage();
+        }
+    }
+
+    public void init()
+    {
+        if (schemas == null || schemas.size() == 0) {
+            schemas = Arrays.asList(userName);
+        }
+
+        schemaSelector = joinForSql(schemas);
+    }
+
+    private static String joinForSql(List<String> identifiers)
+    {
+        StringBuilder builder = new StringBuilder();
+
+        if (identifiers != null) {
+            for (String str : identifiers) {
+                if (builder.length() > 0) {
+                    builder.append(",");
+                }
+                builder.append("'");
+                builder.append(str.toUpperCase());
+                builder.append("'");
+            }
+        }
+        return builder.toString();
+    }
+    
+    public void run() throws IOException
+    {
+        final PrintWriter output;
+
+        if (outputFile == null) {
+            output = new PrintWriter(System.out);
+        }
+        else {
+            output = new PrintWriter(new BufferedWriter(new FileWriter(outputFile)));
+        }
+        
+        IDBI dbi = new DBI(jdbcUrl, userName, password);
+
+        dbi.withHandle(new HandleCallback<Void>() {
+            @Override
+            public Void withHandle(Handle handle) throws Exception
+            {
+                dumpUsers(handle, output);
+                dumpTables(handle, output);
+                dumpDdlForObjectTypes(handle, NON_TABLE_TYPES, output);
+                if (dataTables != null) {
+                    for (String dataTable : dataTables) {
+                        dumpDataForTable(handle, dataTable, output);
+                    }
+                }
+                return null;
+            }
+        });
+        output.close();
+    }
+
+    private void dumpUsers(final Handle handle, final PrintWriter output)
+    {
+        String ddl = handle.createQuery("SELECT trim(dbms_metadata.get_ddl('USER', username)) || '/' FROM all_users WHERE username IN (" + schemaSelector + ")")
+                           .map(StringMapper.FIRST).first();
+        if (ddl != null) {
+            output.println(ddl + "\n");
+        }
+        ddl = handle.createQuery("SELECT trim(dbms_metadata.get_granted_ddl('ROLE_GRANT', username)) || '/' FROM all_users WHERE username IN (" + schemaSelector + ")")
+                    .map(StringMapper.FIRST).first();
+        if (ddl != null) {
+            output.println(ddl + "\n");
+        }
+        ddl = handle.createQuery("SELECT trim(dbms_metadata.get_granted_ddl('SYSTEM_GRANT', username)) || '/' FROM all_users WHERE username IN (" + schemaSelector + ")")
+                    .map(StringMapper.FIRST).first();
+        if (ddl != null) {
+            output.println(ddl + "\n");
+        }
+        ddl = handle.createQuery("SELECT trim(dbms_metadata.get_granted_ddl('OBJECT_GRANT', username)) || '/' FROM all_users WHERE username IN (" + schemaSelector + ")")
+                    .map(StringMapper.FIRST).first();
+        if (ddl != null) {
+            output.println(ddl + "\n");
+        }
+    }
+
+    private void dumpTables(Handle handle, final PrintWriter output)
+    {
+        ResultIterator<Map<String, Object>> constraintIt = handle.createQuery("SELECT table_name, constraint_name FROM all_constraints " +
+                                                                              "  WHERE owner IN (" + schemaSelector + ") " +
+                                                                              "    AND constraint_type IN ('P', 'U', 'R')")
+                                                                 .setFetchSize(1000)
+                                                                 .iterator();
+        DefaultDirectedGraph<String, DefaultEdge> dag = new DefaultDirectedGraph<String, DefaultEdge>(DefaultEdge.class);
+        Set<String> tables = new HashSet<String>();
+        Map<String, String> constraintsForTables = new HashMap<String, String>();
+
+        while (constraintIt.hasNext()) {
+            Map<String, Object> constraint = constraintIt.next();
+            String tableName = constraint.get("table_name").toString();
+            String constraintName = constraint.get("constraint_name").toString();
+
+            if (!tables.contains(tableName)) {
+                dag.addVertex(tableName);
+                tables.add(tableName);
+            }
+            constraintsForTables.put(constraintName, tableName);
+        }
+        constraintIt.close();
+        constraintIt = handle.createQuery("SELECT table_name, constraint_name, r_constraint_name FROM all_constraints " +
+                                          "  WHERE owner IN (" + schemaSelector + ") " +
+                                          "    AND constraint_type IN ('R')")
+                             .setFetchSize(1000)
+                             .iterator();
+
+        while (constraintIt.hasNext()) {
+            Map<String, Object> constraint = constraintIt.next();
+            String tableName = constraint.get("table_name").toString();
+            String constraintName = constraint.get("constraint_name").toString();
+            String remoteConstraintName = constraint.get("r_constraint_name").toString();
+            String remoteTable = constraintsForTables.get(remoteConstraintName);
+
+            if (remoteTable == null) {
+                System.err.println("Constraint " + constraintName + " references a remote constraint not in the current schema: " + remoteConstraintName);
+            }
+            else if (!remoteTable.equalsIgnoreCase(tableName)) {
+                dag.addEdge(remoteTable, tableName);
+            }
+        }
+        constraintIt.close();
+
+        CycleDetector<String, DefaultEdge> cycleDetector = new CycleDetector<String, DefaultEdge>(dag);
+
+        if (cycleDetector.detectCycles()) {
+            Set<String> cycles = cycleDetector.findCycles();
+            while (!cycles.isEmpty()) {
+                String cycle = cycles.iterator().next();
+                Set<String> subCycle = cycleDetector.findCyclesContainingVertex(cycle);
+
+                System.err.println("Found cyclic dependency: " + cycle);
+                for (String sub : subCycle) {
+                   System.err.println("   " + sub);
+                   cycles.remove(sub);
+                }
+            }
+        }
+        else {
+            TopologicalOrderIterator<String, DefaultEdge> orderIterator = new TopologicalOrderIterator<String, DefaultEdge>(dag);
+
+            while (orderIterator.hasNext()) {
+                dumpDdlForObject(handle, orderIterator.next(), output);
+            }
+        }
+    }
+
+    private void dumpDdlForObjectTypes(final Handle handle, final List<String> types, final PrintWriter output)
+    {
+        ResultIterator<Map<String, Object>> resultIt = handle.createQuery("SELECT object_type type, object_name name FROM all_objects " +
+                                                                          "  WHERE owner IN (" + schemaSelector + ") " +
+                                                                          "    AND object_type IN (" + joinForSql(types) + ") " +
+                                                                          "  ORDER BY object_type DESC")
+                                                             .setFetchSize(1000)
+                                                             .iterator();
+        
+        while (resultIt.hasNext()) {
+            Map<String, Object> row = resultIt.next();
+            String type = row.get("type").toString();
+            String name = row.get("name").toString();
+            
+            String ddl = handle.createQuery("SELECT trim(dbms_metadata.get_ddl('" + type + "','" + name + "')) || '/' FROM DUAL")
+                               .map(StringMapper.FIRST).first();
+            if (ddl != null) {
+                output.println(ddl + "\n");
+            }
+        }
+        resultIt.close();
+    }
+    
+    private void dumpDdlForObject(final Handle handle, final String objectName, final PrintWriter output)
+    {
+        Map<String, Object> result = handle.createQuery("SELECT object_type type, object_name name FROM all_objects " +
+                                                        "  WHERE owner IN (" + schemaSelector + ") " +
+                                                        "    AND object_name = :object_name" +
+                                                        "  ORDER BY object_type DESC")
+                                           .bind("object_name", objectName.toUpperCase())
+                                           .first();
+
+        if (result != null) {
+            String type = result.get("type").toString();
+            String name = result.get("name").toString();
+
+            String ddl = handle.createQuery("SELECT trim(dbms_metadata.get_ddl('" + type + "','" + name + "')) || '/' FROM DUAL")
+                               .map(StringMapper.FIRST).first();
+            if (ddl != null) {
+                output.println(ddl + "\n");
+            }
+        }
+    }
+
+    private void dumpDataForTable(final Handle handle, final String table, final PrintWriter output) throws SQLException
+    {
+        String realTableName = handle.createQuery("SELECT object_name FROM all_objects " +
+                                                  "  WHERE owner IN (" + schemaSelector + ") " +
+                                                  "    AND object_name = :object_name" +
+                                                  "    AND object_type = 'TABLE'")
+                                     .bind("object_name", table.toUpperCase())
+                                     .map(StringMapper.FIRST)
+                                     .first();
+
+        if (realTableName == null) {
+            System.err.println("The table " + table + " does not exist, is not a table or is not owned by one of the specified users");
+            return;
+        }
+        
+        final String sql = "SELECT * FROM " + table;
+
+        ResultSet rs = handle.getConnection().prepareStatement(sql).executeQuery();
+        ResultSetMetaData metaData = rs.getMetaData();
+        String[] columnNames = new String[metaData.getColumnCount()];
+        int[] columnTypes = new int[metaData.getColumnCount()];
+
+        for (int columnIdx = 0; columnIdx < columnNames.length; columnIdx++) {
+            columnNames[columnIdx] = metaData.getColumnName(columnIdx + 1);
+            columnTypes[columnIdx] = metaData.getColumnType(columnIdx + 1);
+        }
+
+        StringBuilder insertStmtFormatBuilder = new StringBuilder();
+
+        insertStmtFormatBuilder.append("INSERT INTO \"");
+        insertStmtFormatBuilder.append(realTableName);
+        insertStmtFormatBuilder.append("\" (\"");
+
+        for (int columnIdx = 0; columnIdx < columnNames.length; columnIdx++) {
+            if (columnIdx > 0) {
+                insertStmtFormatBuilder.append("\",\"");
+            }
+            insertStmtFormatBuilder.append(columnNames[columnIdx]);
+        }
+        insertStmtFormatBuilder.append("\") VALUES (%s) /");
+
+        String insertStmtFormat = insertStmtFormatBuilder.toString();
+        StringBuilder valuesBuilder = new StringBuilder();
+
+        while (rs.next()) {
+            valuesBuilder.setLength(0);
+            for (int columnIdx = 0; columnIdx < columnNames.length; columnIdx++) {
+                String nextValue = null;
+
+                switch (columnTypes[columnIdx]) {
+                    case Types.BIGINT:
+                    case Types.BIT:
+                    case Types.BOOLEAN:
+                    case Types.DECIMAL:
+                    case Types.DOUBLE:
+                    case Types.FLOAT:
+                    case Types.INTEGER:
+                    case Types.NUMERIC:
+                    case Types.REAL:
+                    case Types.SMALLINT:
+                    case Types.TINYINT:
+                        nextValue = rs.getString(columnIdx + 1);
+                        break;
+                    case Types.DATE:
+                        nextValue = toString(rs.getDate(columnIdx + 1));
+                        break;
+                    case Types.TIME:
+                        nextValue = toString(rs.getTime(columnIdx + 1));
+                        break;
+                    case Types.TIMESTAMP:
+                        nextValue = toString(rs.getTimestamp(columnIdx + 1));
+                        break;
+                    default:
+                        nextValue = rs.getString(columnIdx + 1);
+                        if (nextValue != null) {
+                            nextValue = "'" + nextValue.replaceAll("'", "''") + "'";
+                        }
+                        break;
+                }
+                if (columnIdx > 0) {
+                    valuesBuilder.append(",");
+                }
+                if (nextValue == null) {
+                    valuesBuilder.append("null");
+                }
+                else {
+                    valuesBuilder.append(nextValue);
+                }
+            }
+            output.println(String.format(insertStmtFormat, valuesBuilder.toString()));
+        }
+        rs.close();
+    }
+
+    private static String toString(Date date)
+    {
+        return date == null ? null :  "TO_DATE('" + ORACLE_DATE_FORMAT.format(date) + "', 'YYYY/MM/DD HH24:MI:SS')";
+    }
+}
